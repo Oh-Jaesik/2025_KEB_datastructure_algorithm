5296fe9d95d7a0f4a9fda341bad8f145ff713b38
Oh-jaesik <ojs72272@gmail.com>
2025-02-17 14:05:59 +0900

    v0.7 day06 poly

--- day06.py
+++ day06.py
@@ -1,6 +1,34 @@
-n = int(input())
-sum = 0
-for i in range(1, n+1):
-    sum += i
+def print_poly(f_x) -> str:
+    term = len(f_x) - 1
+    poly_expression = "f(x) = "
 
-print(sum)
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+
+        if coefficient >= 0:
+            poly_expression = poly_expression + "+"
+        poly_expression = poly_expression + f'{coefficient}x^{term} '
+        term = term - 1
+
+    return poly_expression
+
+
+def calculation_poly(x_value, f_x) -> int:
+    return_value = 0
+    term = len(f_x) - 1
+
+    for i in range(len(fx)):
+        coefficient = f_x[i]
+        return_value += coefficient * pow(x_value, term)
+        term = term - 1
+
+    return return_value
+
+
+# fx = [2, 3, 4, 0, -9]
+fx = [2, 5, -9, 11]
+tx = [20, 7, 2, 0]
+
+if __name__ == "__main__":
+    print(print_poly(fx))
+    print(calculation_poly(int(input("x 값 : ")), fx))
