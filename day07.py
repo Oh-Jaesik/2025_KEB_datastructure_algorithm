6f18daa9950067cf1c184496d903784264dd1f12
Oh-jaesik <ojs72272@gmail.com>
2025-02-18 13:45:27 +0900

    v2.4 day07 linked list

--- day07.py
+++ day07.py
@@ -1,3 +1,5 @@
+import random
+
 class Node:
     def __init__(self, data, next = None):
         self.data = data
@@ -24,8 +26,48 @@ class LinkedList:
             node = node.next
         return "end"
 
+    def search(self, target):
+        current = self.head
+        while current.next:
+            if current.data == target:
+                return True
+            else:
+                current = current.next
+        return False
+
+    def remove(self, target):
+        if self.head.data == target:
+            self.head = self.head.next
+            return
+        current = self.head
+        previous = None
+        while current:
+            if current.data == target:
+                previous.next = current.next
+                break
+            else:
+                previous = current
+                current = current.next
+
 if __name__ == "__main__":
-    l = LinkedList()
+    l  = LinkedList()
     l.append(7)
+    l.append(-11)
     l.append(8)
     print(l)
+    l.remove(7)
+    print(l)
+
+    # l = LinkedList()
+    # # l.append(7)
+    # # l.append(8)
+    # i = 0
+    # while i <20:
+    #     n = random.randint(1, 20)
+    #     l.append(n)
+    #     print(n, end = " ")
+    #     i = i+1
+    #
+    # print()
+    # print(l)
+    # print(l.search(10))
